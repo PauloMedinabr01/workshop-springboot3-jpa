41caa972c458b4cd62e1b930ac66a56d38cb1c3d
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-03-09 14:00:22 -0300

    OrderItem, many-to-many association with extra attributes Part 2

--- src/main/java/com/educandoweb/curso/entities/Order.java
+++ src/main/java/com/educandoweb/curso/entities/Order.java
@@ -2,7 +2,6 @@ package com.educandoweb.curso.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-
 import com.educandoweb.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
@@ -11,7 +10,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "tb_order")
@@ -30,6 +32,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
 
@@ -74,6 +79,10 @@ public class Order implements Serializable {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
--- src/main/java/com/educandoweb/curso/entities/OrderItem.java
+++ src/main/java/com/educandoweb/curso/entities/OrderItem.java
@@ -1,6 +1,8 @@
 package com.educandoweb.curso.entities;
 
 import com.educandoweb.curso.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,7 +14,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -28,6 +30,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
